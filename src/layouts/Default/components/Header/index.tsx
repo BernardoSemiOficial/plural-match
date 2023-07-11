4aad0566e9f495ca9b3442f78847f1b1169671f3
BernardoSemiOficial <bernardopereiraoliveira258@gmail.com>
2023-07-11 20:47:25 -0300

    feat: create header

--- src/layouts/Default/components/Header/index.tsx
+++ src/layouts/Default/components/Header/index.tsx
@@ -3,7 +3,9 @@ import * as S from './header.css'
 export const Header = () => {
   return (
     <header className={S.container}>
-      <div>header</div>
+      <div className={S.content}>
+        <h1 className={S.title}>Plural Match</h1>
+      </div>
     </header>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
           crossOrigin=''
         />
         <link
-          href='https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap'
+          href='https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap'
           rel='stylesheet'
         />
       </Head>
